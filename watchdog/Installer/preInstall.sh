e1395aebf4dab45525b3171bcace99301f4a27a3
Samuel N Cui <i@samcui.com>
2023-10-30 11:58:42 +0800

    feat: remove non lsi code

--- watchdog/Installer/preInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/bin/rm -rf '/System/Library/Extensions/iTCOWatchdog.kext' \
-            '/usr/local/libexec/Watchdog Feeder' \
-            '/Library/LaunchDaemons/cc.dukzcry.WatchdogFeeder.plist'
-
-/usr/bin/killall 'Watchdog Feeder' 2>&1 >/dev/null
-sleep 2
